93c5264285cc358505f7b473636c6c54d75fcf25
Eric Eaton <ericthomaseaton@gmail.com>
2022-08-09 11:02:35 -0400

    Change entire website
    Massive overhaul. Changed to vaporwave/cyberpunk aesthetics.

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
-let counter = document.getElementById("counter")
-let count = 0
-counter.textContent = count
+let everything = document.getElementById("columns")
+let scanBtn = document.getElementById("scan-btn")
+let caesar = document.getElementById("caesar")
+let accessDenied = document.getElementById("access-denied")
+let createProfile = document.getElementById("create-profile")
 
-function subtract() {
-    count -= 1
-    counter.textContent = count
+function scan() {
+    createProfile.style.display = "block"
+    accessDenied.style.display = "none"
+    scanBtn.style.display = "none"
+    caesar.style.display = "block"
+    function scanComplete() {
+        caesar.style.display = "none"
+        everything.style.visibility = "visible"
+        createProfile.style.display = "none"
+    }
+    setTimeout(scanComplete, 3000)
 }
 
-function add() {
-    count += 1
-    counter.textContent = count
+let glassOfWater = document.getElementById("glass-of-water")
+
+function drinkWater() {
+    glassOfWater.style.display = "block"
 }
+
+let weapons = document.getElementById("weapons")
+let weaponsHeader = document.getElementById("weapons-header")
+
+function manufactureWeapons() {
+    weapons.style.display = "block"
+    weaponsHeader.textContent = "//Protocol 'lock-n-load.exe' intializing..."
+}
+
